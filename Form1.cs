fcb05af283313aca4f50781632ff017c9366b67c
subin1848 <s2315@e-mirim.hs.kr>
2025-08-27 10:02:01 +0900

    #8 내역 저장하는 SaveHistory 메서드 구현

--- Form1.cs
+++ Form1.cs
@@ -94,7 +94,25 @@ namespace FortuneTeller
             tbResult.Text = birthday + " " + birthtime + Environment.NewLine 
                 + saju + Environment.NewLine
                 + message;
-            
+            SaveHistory($"{birthday} {birthtime}|{result}");
+        }
+
+        private void SaveHistory(string history)
+        {
+            try
+            {
+                string filename = "history.csv";
+                File.AppendAllText(filename, history + Environment.NewLine);
+            } 
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"권한이 없습니다. \n{ex.Message}", "권한 오류!");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류가 발생했습니다. \n{ex.Message}", "알 수 없는 오류!");
+            }
         }
     }
 }
