6840e9ac3d682332a4341f234d5cc14be8530bb6
subin1848 <s2315@e-mirim.hs.kr>
2025-08-27 09:17:11 +0900

    #5 LoadResults 메서드 구현

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,33 @@ namespace FortuneTeller
         public Form1()
         {
             InitializeComponent();
+            LoadResults();
+        }
+
+        private void LoadResults()
+        {
+            try
+            {
+                string filename = "results.csv";
+                results = File.ReadAllLines(filename).ToList();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show($"파일이 존재하지 않습니다. \n{ex.Message}", "파일이 없는 오류!");
+            }
+            
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"권한이 없습니다. \n{ex.Message}", "파일 권한 오류!");
+            }
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류가 발생했습니다. \n{ex.Message}", "알 수 없는 오류!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
         }
 
         private void label2_Click(object sender, EventArgs e)
